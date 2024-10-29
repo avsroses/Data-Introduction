c926475b744096251cc0407df80af62c8c2e3659
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-29 15:14:59 +0000

    favourites add images

--- js/favourites.js
+++ js/favourites.js
@@ -5,18 +5,21 @@ const favouritesDiv = document.getElementById("favourites-div");
 
 // current image stores image currently displayed
 let currentImage = undefined;
+// list of all favourites used to manage repetition
+let favouritesList = [];
 
 /**
  * create new image element and append it to the favourites div
  */
 function onAddFavouriteButtonClick() {
+    // check currently displayed list not in list already
+    if (favouritesList.includes(currentImage)) return;
+    // add current image to favourites list
+    favouritesList.push(currentImage);
+
     const newImage = document.createElement("img");
     newImage.src = currentImage;
     newImage.classList.add("favourite-img");
-
-    if (newImage.src != currentImage.src){
-        favouritesDiv.append(newImage);
-    }
 }
 
 addFavouriteButton.onclick = onAddFavouriteButtonClick;
